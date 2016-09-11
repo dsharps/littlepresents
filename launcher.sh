bcfe6e68833d28857cea379fadc7cb4096db2b95
dsharps <davidrsharples@gmail.com>
2016-09-10 20:58:38 -0700

    First finish, made a startup script

---
+++ launcher.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to this directory, then execute py script, then home
+
+cd /
+cd home/pi/Projects/littlepresents
+sudo python littlepresents.py
+cd /
